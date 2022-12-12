560f38a266211786e3d264f4582a929fe798f263
Адиль Касымбеков <adkasyv@MacBook-Air-Adil.local>
2022-12-12 09:37:17 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import "macro-css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="class1">
+        1111
+        <div className="mt-30">2222</div>
+      </div>
     </div>
   );
 }

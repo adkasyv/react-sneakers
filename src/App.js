64a1b4525d70680b0c249892b0b40e3745f74077
Адиль Касымбеков <adkasyv@MacBook-Air-Adil.local>
2022-12-12 13:33:29 +0600

    Add folder components(Card, Drawer, Header), editing item card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import "./App.scss";
-import "macro-css";
+import Card from "./components/Card/Card";
+import Header from "./components/Header";
+import Drawer from "./components/Drawer";
+
+const arr = [
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 12999,
+    imageUrl: "/img/sneakers/1.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 15600,
+    imageUrl: "/img/sneakers/2.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8499,
+    imageUrl: "/img/sneakers/3.jpg",
+  },
+  {
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 8999,
+    imageUrl: "/img/sneakers/4.jpg",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <div className="class1">
-        1111
-        <div className="mt-30">2222</div>
+    <div className="wrapper clear">
+      <Drawer />
+      <Header />
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
+
+        <div className="d-flex">
+          {arr.map((obj) => (
+            <Card
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+              onClick={() => console.log(obj)}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,26 @@
+import styles from "./Card.module.scss";
+
+console.log(styles);
+
+function Card(props) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.favorite}>
+        <img src="/img/heart-unliked.svg" alt="Unliked" />
+      </div>
+      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
+      <h5>{props.title}</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена:</span>
+          <b>{props.price} руб.</b>
+        </div>
+        <button className="button" onClick={props.onClick}>
+          <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;

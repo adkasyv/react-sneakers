f117381e21b5fcfde4036c0dbed3bcbe12f58082
Адиль Касымбеков <adkasyv@MacBook-Air-Adil.local>
2022-12-13 13:26:18 +0600

    Added check mark in card and shopping basket

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Card from "./components/Card/Card";
+import React from "react";
+import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
@@ -26,10 +27,15 @@ const arr = [
 ];
 
 function App() {
+  const [cartItems, setCartItems] = React.useState([]);
+  const [cartOpened, setCartOpened] = React.useState(false);
+
   return (
     <div className="wrapper clear">
-      <Drawer />
-      <Header />
+      {cartOpened && (
+        <Drawer items={cartItems} onClose={() => setCartOpened(false)} />
+      )}
+      <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
           <h1>Все кроссовки</h1>
---
+++ src/components/Card/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./Card.module.scss";
+
+function Card({ title, imageUrl, price, onFavorite, onPlus }) {
+  const [isAdded, setIsAdded] = React.useState(false);
+
+  const onClickPlus = () => {
+    setIsAdded(!isAdded);
+  };
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.favorite}>
+        <img src="/img/heart-unliked.svg" alt="Unliked" />
+      </div>
+      <img width={133} height={112} src={imageUrl} alt="Sneakers" />
+      <h5>{title}</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена:</span>
+          <b>{price} руб.</b>
+        </div>
+        <img
+          className={styles.plus}
+          onClick={onClickPlus}
+          // width={11}
+          // height={11}
+          src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
